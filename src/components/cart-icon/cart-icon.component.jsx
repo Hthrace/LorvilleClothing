de747ff00623a418118fd9551c767b3af211283d
Hailey Thrace <hthrace@gmail.com>
2023-09-23 21:45:02 -0400

    update function name

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,9 +5,9 @@ import { CartContext } from "../../contexts/cart.context";
 
 const CartIcon = () => {
   const {cart, setCart} = useContext(CartContext)
-  const toggleActiveCart = () => setCart((curr)=>{return {...curr, isActive:!curr.isActive}})
+  const toggleCart = () => setCart((curr)=>{return {...curr, isActive:!curr.isActive}})
   return (
-    <div className="cart-icon-container" onClick={toggleActiveCart}>
+    <div className="cart-icon-container" onClick={toggleCart}>
       <img src={ShopingBag} alt="" className="shopping-icon"/>
       <span className="item-count">{cart.itemCount}</span>
     </div>
