065165fcb97dfc389de0be55b5801b7001bac67a
Hailey Thrace <hthrace@gmail.com>
2023-10-03 16:52:27 -0400

    update media query sign inbuttons

--- src/components/sign-in/sign-in-form.styles.jsx
+++ src/components/sign-in/sign-in-form.styles.jsx
@@ -23,6 +23,9 @@ export const ButtonsContainer = styled.div`
 
   @media screen and (max-width: 600px) {
     width: 85vw;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
     grid-gap: 20px;
   }
 `;
