ba9a9b375b3cb40acc0c1e2b64917b3f613d1665
Hailey Thrace <hthrace@gmail.com>
2024-02-24 02:16:13 -0500

    update env to vite env

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,2 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
-export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
+
+export const stripePromise = loadStripe(
+    import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY
+);
